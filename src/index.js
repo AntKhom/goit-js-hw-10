cc1ba7700cbb7ebb6b1186ddaedade2be9a67d5d
AntKhom <antonhomenko1988@gmail.com>
2023-09-07 09:41:12 -0400

    ready without lib

--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@
 // console.log(catsBreeds);
 
 import axios from "axios"
+import SlimSelect from 'slim-select'
+
+import "./css/slimselect.css"
+
+
 
 axios.defaults.headers.common["x-api-key"] = "live_EwkjZUK3En2Yoij9vB5n4Ebg3dZ1XSbsjtmjkUxXGXLkfvuRYXjwDvhi9BdcueHu";
 const baseURL = "https://api.thecatapi.com/v1/breeds";
 const imageURL = "https://api.thecatapi.com/v1/images/search"
 
 const catsSelector = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+const messageLoader = document.querySelector('.loader');
+const messageError = document.querySelector('.error')
+
+const changeVisibility = (item) => {
+    item.classList.toggle("invisible");
+};
+changeVisibility(messageError);
+changeVisibility(messageLoader);
 
 const fetchBreeds = () => {
     return axios.get(baseURL)
@@ -20,164 +34,71 @@ const fetchBreeds = () => {
         .catch((error) => console.log(error))
 };  
 
-const fetchCatByBreed = (breedId) => {
-    const param = {
-        breed_ids: breedId,
-    }
-    return axios.get(imageURL, { params: param })
-        .then((response) => {
-            console.log(response.data)
-            return response.data;
-    })
-        .catch((error) => console.log(error))
-}
-
 fetchBreeds()
     .then((data) => {
         const nameCats = data
             .map((arr) => `<option value="${arr.id}">${arr.name}</option>`)
             .join('');
-        catsSelector.innerHTML=nameCats;
+        catsSelector.innerHTML = nameCats;
+        new SlimSelect({
+            select: '.breed-select',
+        });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+        console.log(err)
+        changeVisibility(messageError);
+    });
 
-fetchCatByBreed('abys')
-    .then((data) => {
-        console.log(data);        
+const fetchCatByBreed = (breedId) => {
+    const param = {
+        limit: 1,
+        breed_ids: breedId,
+    }
+    return axios.get(imageURL, { params: param })
+        .then((response) => {
+            console.log(response.data[0])
+            return response.data[0];
     })
-    .catch((err) => console.log(err));
-// class App extends Component {
+        .catch((error) => console.log(error))
+}
 
-//   getBreeds() {
-//       const res = axios('/breeds');
-//       return res.data;
-//   }
-//   getCatsImagesByBreed(breed_id) {
-//       const res = axios('/images/search?breed_ids='+breed_id);
-      
-//       console.table(res.data)
-//       return res.data;
-//   }
 
-//   loadBreedImages() {
-//     console.log('Load Breed Images:', this.state.selected_breed)
-
-//     let breed_images = this.getCatsImagesByBreed(this.state.selected_breed, 5)
-
-//     this.setState({ images: breed_images });
-//   }
-
-//   constructor(...args) {
-
-//       super(...args);
-//       this.state = {
-//         images: [],
-//         breeds: [], 
-//         selected_breed: 0
-//       };
-
-//     this.onBreedSelectChange = this.onBreedSelectChange.bind(this);
-//   }
-//   onBreedSelectChange(e) {
-//     console.log("Breed Selected. ID:",e.target.value)
-//     this.setState({selected_breed:e.target.value});
-//     this.loadBreedImages();
-//   }
-//   componentDidMount() {
-//       if (this.state.breeds.length===0) {
-//           (() => {
-//               try {
-//                   this.setState({breeds: this.getBreeds()});
-//               } catch (e) {
-//                   //...handle the error...
-//                   console.error(e)
-//               }
-//           })();
-//       }
-//   }
-//   render() {
-//       return (
-//         <div>
-
-//       <select value={this.state.selected_breed} 
-//               onChange={this.onBreedSelectChange}>
-//         {this.state.breeds.map((breed) => <option key={breed.id} value={breed.id}>{breed.name}</option>)}
-//       </select>
-
-//       <div>
-//      {this.state.images.map((image) => <img className="cat-image" alt="" src={image.url}></img>)}
-//      </div>
-
-//      </div>
-//       );
-//   }
-// }
-
-// export default App;
+const choiseCatHandler = (e) => {
+    console.log(e.target.value); 
+    const catId = e.target.value;
+    changeVisibility(messageLoader);
+    fetchCatByBreed(catId)
+    .then((dataCat) => {
+        console.log(dataCat.breeds[0]);
+        changeVisibility(messageLoader);
+        createMarkup(dataCat, catInfo);
+    })
+        .catch((err) => {
+            console.log(err);
+            changeVisibility(messageError);
+        });
+}
 
 
 
 
 
+const createMarkup = (item, container) => {
+    const markup = 
+    `
+    <div><img class=breed-image src='${item.url}' width=600 /></div>
+    <div>
+    <h2 class=breed-name>${item.breeds[0].name}</h2>
+    <p class=breed-description>${item.breeds[0].description}</p>
+    <p><span class=breed-temperament>Temperament: </span>${item.breeds[0].temperament}</p>
+    </div>
+    `
+    container.innerHTML = markup;    
+}
 
 
 
 
-//     // const url = `https://api.thecatapi.com/v1/breeds`;
-//     // const api_key = "live_EwkjZUK3En2Yoij9vB5n4Ebg3dZ1XSbsjtmjkUxXGXLkfvuRYXjwDvhi9BdcueHu"
-// let storedBreeds = [];
-
-// 
-// const catInfo = document.querySelector('.cat-info');
-
-// fetch(url,{headers: {
-//       'x-api-key': api_key
-//     }})
-// .then((response) => {
-//    return response.json();
-//  })
-// .then((data) => {
-//     console.log(data);
-//    //filter to only include those with an `image` object
-//     data = data.filter(img=> img.image?.url!=null)
-    
-//     storedBreeds = data;
-   
-//     for (let i = 0; i < storedBreeds.length; i++) {
-//         const breed = storedBreeds[i];
-//         let option = document.createElement('option');
-//         //skip any breeds that don't have an image
-//         if(!breed.image)continue
-     
-//         //use the current array index
-//         option.value = i;
-//         option.innerHTML = `${breed.name}`;
-//         catSelector.appendChild(option);
-//     }
-//    //show the first breed by default
-//     createMarkupHandler(12, catInfo);
-//    showBreedImage(0)
-// })
-// .catch(function(error) {
-//    console.log(error);
-// });
+catsSelector.addEventListener('change',choiseCatHandler)
 
 
-// const createMarkupHandler=(i, container)=> {
-//     markup = `
-//     <div><img class=breed-image src='${storedBreeds[i].image.url}'/></div>
-//     <div><h2 class=breed-name>${storedBreeds[i].name}</h2>
-//     <p></p>
-//     <p>Temperament:<span class=breed-temperament>${storedBreeds[i].temperament}</span></p></div>
-//     `
-//     container.innerHTML = markup;
-// }
-
-
-// function showBreedImage(index)
-// { 
-//   document.getElementById("breed_image").src= storedBreeds[index].image.url;
-//   document.getElementById("breed_json").textContent= storedBreeds[index].temperament
-//   document.getElementById("wiki_link").href= storedBreeds[index].wikipedia_url
-//   document.getElementById("wiki_link").innerHTML= storedBreeds[index].wikipedia_url
-// }
