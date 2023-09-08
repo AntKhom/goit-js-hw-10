379b28382a714d611335bb7cb529cc7b638694e2
AntKhom <antonhomenko1988@gmail.com>
2023-09-08 11:13:01 -0400

    ready

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-// import { catsBreeds } from "./js/cat-api";
+import apiCats from "./js/cat-api";
 
-// console.log(catsBreeds);
-
-import axios from "axios"
 import SlimSelect from 'slim-select'
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
 
+//css
 import "./css/slimselect.css"
 
-
-
-axios.defaults.headers.common["x-api-key"] = "live_EwkjZUK3En2Yoij9vB5n4Ebg3dZ1XSbsjtmjkUxXGXLkfvuRYXjwDvhi9BdcueHu";
-const baseURL = "https://api.thecatapi.com/v1/breeds";
-const imageURL = "https://api.thecatapi.com/v1/images/search"
 
 const catsSelector = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
@@ -21,69 +15,54 @@ const messageError = document.querySelector('.error')
 const changeVisibility = (item) => {
     item.classList.toggle("invisible");
 };
-changeVisibility(messageError);
-changeVisibility(messageLoader);
 
-const fetchBreeds = () => {
-    return axios.get(baseURL)
-        .then((response) => {
-            const resData = response.data;
-            console.log(resData);
-            return resData;
-        })   
-        .catch((error) => console.log(error))
-};  
+const createListNameCats = (data) => {
+    if (!data) {
+        return;
+    }
+    const nameCats = data
+        .map((arr) => `<option value="${arr.id}">${arr.name}</option>`)
+        .join('');
+    catsSelector.innerHTML = nameCats;
+    new SlimSelect({
+        select: '.breed-select',
+    });
+};
 
-fetchBreeds()
+apiCats.fetchBreeds()
     .then((data) => {
-        const nameCats = data
-            .map((arr) => `<option value="${arr.id}">${arr.name}</option>`)
-            .join('');
-        catsSelector.innerHTML = nameCats;
-        new SlimSelect({
-            select: '.breed-select',
-        });
+        createListNameCats(data);
     })
     .catch((err) => {
         console.log(err)
         changeVisibility(messageError);
     });
 
-const fetchCatByBreed = (breedId) => {
-    const param = {
-        limit: 1,
-        breed_ids: breedId,
-    }
-    return axios.get(imageURL, { params: param })
-        .then((response) => {
-            console.log(response.data[0])
-            return response.data[0];
-    })
-        .catch((error) => console.log(error))
-}
-
 
 const choiseCatHandler = (e) => {
-    console.log(e.target.value); 
+    //console.log(e.target.value); 
     const catId = e.target.value;
-    changeVisibility(messageLoader);
-    fetchCatByBreed(catId)
-    .then((dataCat) => {
-        console.log(dataCat.breeds[0]);
-        changeVisibility(messageLoader);
-        createMarkup(dataCat, catInfo);
-    })
+    catInfo.classList.add("invisible");
+    //changeVisibility(messageLoader);
+    Loading.standard(messageLoader.textContent);
+    apiCats.fetchCatByBreed(catId)
+        .then((dataCat) => {
+            console.log(dataCat.breeds[0]);
+            //changeVisibility(messageLoader);
+            Loading.remove();
+            changeVisibility(catInfo);
+            renderCatInfo(dataCat, catInfo);
+        })
         .catch((err) => {
             console.log(err);
-            changeVisibility(messageError);
+            messageError.classList.remove("invisible")   
+            //changeVisibility(messageLoader);
+            Loading.remove();
         });
 }
 
 
-
-
-
-const createMarkup = (item, container) => {
+const renderCatInfo = (item, container) => {
     const markup = 
     `
     <div><img class=breed-image src='${item.url}' alt='foto ${item.breeds[0].name}' width=100%/></div>
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,15 +1,37 @@
 import axios from "axios"
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 axios.defaults.headers.common["x-api-key"] = "live_EwkjZUK3En2Yoij9vB5n4Ebg3dZ1XSbsjtmjkUxXGXLkfvuRYXjwDvhi9BdcueHu";
 const baseURL = "https://api.thecatapi.com/v1/breeds";
+const imageURL = "https://api.thecatapi.com/v1/images/search"   
 
-const fetchBreeds = () => {
-    return axios.get(baseURL)
-        .then((response) => {
-            const resData = response.data;
-            return resData;
-        })   
-        .catch((error) => console.log(error))
-};  
+const apiCats = {
+    fetchBreeds () {
+        return axios.get(baseURL)
+            .then((response) => {
+                if (response) {
+                    const resData = response.data;
+                    console.log(resData);
+                    return resData;
+                }
+            })
+            .catch((error) => Notify.failure(error.message))
+    },
 
-export const catsBreeds =  fetchBreeds().then((data) => { return data.map(()=>data.name) }).catch((err) => console.log(err));
+    fetchCatByBreed (breedId) {
+        const param = {
+            limit: 1,
+            breed_ids: breedId,
+        }
+        return axios.get(imageURL, { params: param })
+            .then((response) => {
+                if (response) {
+                    console.log(response.data[0])
+                    return response.data[0];
+                }
+            })
+            .catch((error) => Notify.failure(error.message))
+    },
+};
+
+export default apiCats;
